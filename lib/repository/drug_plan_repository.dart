6c3c3c88e193d7c3726941da9537b43bfe093628
Bruno Campera <brunocmpr@gmail.com>
2023-04-24 16:25:34 -0300

    Implement drug plan service and repository layers

---
+++ lib/repository/drug_plan_repository.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:app3idade_caretaker/exceptions/unauthorized_exception.dart';
+import 'package:app3idade_caretaker/models/drug_plan.dart';
+import 'package:app3idade_caretaker/services/api.dart';
+import 'package:http/http.dart' as http;
+
+class DrugPlanRepository {
+  final baseUri = '/plan';
+
+  Future<DrugPlan> createDrugPLan(DrugPlan drugPlan) async {
+    final headers = API.headerContentTypeJson;
+    headers.addAll(API.headerAuthorization);
+
+    final http.Response response = await http.post(
+      Uri.http(API.url, baseUri),
+      body: drugPlan.toJson(),
+      headers: headers,
+    );
+
+    if (API.isSuccessResponse(response)) {
+      return DrugPlan.fromJson(response.body);
+    } else if (API.isUnauthorizedOrForbiddenResponse(response)) {
+      throw UnauthorizedException(utf8.decode(response.bodyBytes));
+    } else {
+      throw Exception('Erro inserindo usuário: ${response.body}');
+    }
+  }
+}

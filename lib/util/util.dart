6e3765e656f32910643b1f87c4f870ce98eb4f94
Bruno Campera <brunocmpr@gmail.com>
2023-04-30 18:52:29 -0300

    Remove unused code

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 String formatDateTime(DateTime dateTime) => DateFormat('dd/MM/yyyy HH:mm').format(dateTime);
@@ -16,7 +15,7 @@ int? weekdayIntegerFromString(String dayValue) {
   return weekdayInteger;
 }
 
-Map<int, String> dayMap = {
+const Map<int, String> dayMap = {
   1: 'MONDAY',
   2: 'TUESDAY',
   3: 'WEDNESDAY',
@@ -26,76 +25,4 @@ Map<int, String> dayMap = {
   7: 'SUNDAY'
 };
 
-Future<DayOfWeekTime?> showDayOfWeekTimePicker(BuildContext context) async {
-  const daysPtBr = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
-  final List<DropdownMenuItem<int>> daysDropDownItems = daysPtBr
-      .map((day) => DropdownMenuItem<int>(
-            value: (1 + daysPtBr.indexOf(day)),
-            child: Text(day),
-          ))
-      .toList();
-
-  int? selectedDay;
-  return await showModalBottomSheet<DayOfWeekTime>(
-    context: context,
-    builder: (BuildContext context) {
-      return StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
-        TimeOfDay selectedTime = const TimeOfDay(hour: 8, minute: 0);
-        int defaultDay = 1;
-        print(selectedDay);
-        return SizedBox(
-          height: 180,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Text('Selecione um dia e um horário'),
-                    SizedBox(width: 16),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    DropdownButton<int>(
-                      value: selectedDay ?? defaultDay,
-                      items: daysDropDownItems,
-                      onChanged: (int? value) {
-                        if (value == null) return;
-                        setState(() {
-                          selectedDay = value;
-                        });
-                      },
-                    ),
-                    const SizedBox(width: 16),
-                    ElevatedButton(
-                      onPressed: () async {
-                        var navigator = Navigator.of(context);
-                        TimeOfDay? timePicked = await showTimePicker(context: context, initialTime: selectedTime);
-                        if (!navigator.mounted || timePicked == null) return;
-                        selectedTime = timePicked;
-
-                        DayOfWeekTime valuesPicked = DayOfWeekTime(selectedDay ?? defaultDay, timePicked);
-
-                        navigator.pop(valuesPicked);
-                      },
-                      child: const Text('Selecionar horário'),
-                    )
-                  ],
-                )
-              ],
-            ),
-          ),
-        );
-      });
-    },
-  );
-}
-
-class DayOfWeekTime {
-  final int day;
-  final TimeOfDay time;
-  const DayOfWeekTime(this.day, this.time);
-}
+const daysPtBr = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];

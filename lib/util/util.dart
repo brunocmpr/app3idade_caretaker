c231bec7f8ccc4c0aec7d862967016922e09cf80
Bruno Campera <brunocmpr@gmail.com>
2023-04-30 15:55:36 -0300

    Create showDayOfWeekTimePicker modal function

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 String formatDateTime(DateTime dateTime) => DateFormat('dd/MM/yyyy HH:mm').format(dateTime);
@@ -24,3 +25,71 @@ Map<int, String> dayMap = {
   6: 'SATURDAY',
   7: 'SUNDAY'
 };
+
+Future<DayOfWeekTime?> showDayOfWeekTimePicker(BuildContext context) async {
+  const daysPtBr = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+  final List<DropdownMenuItem<int>> daysDropDownItems = daysPtBr
+      .map((day) => DropdownMenuItem<int>(
+            value: (1 + daysPtBr.indexOf(day)),
+            child: Text(day),
+          ))
+      .toList();
+
+  return await showModalBottomSheet<DayOfWeekTime>(
+    context: context,
+    builder: (BuildContext context) {
+      TimeOfDay selectedTime = const TimeOfDay(hour: 8, minute: 0);
+      int selectedDay = 1;
+      return SizedBox(
+        height: 180,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text('Selecione um dia e um horário'),
+                  SizedBox(width: 16),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  DropdownButton<int>(
+                    value: selectedDay,
+                    items: daysDropDownItems,
+                    onChanged: (int? value) {
+                      if (value == null) return;
+                      selectedDay = value;
+                    },
+                  ),
+                  const SizedBox(width: 16),
+                  ElevatedButton(
+                    onPressed: () async {
+                      var navigator = Navigator.of(context);
+                      TimeOfDay? timePicked = await showTimePicker(context: context, initialTime: selectedTime);
+                      if (!navigator.mounted || timePicked == null) return;
+                      selectedTime = timePicked;
+
+                      DayOfWeekTime valuesPicked = DayOfWeekTime(selectedDay, timePicked);
+
+                      navigator.pop(valuesPicked);
+                    },
+                    child: const Text('Selecionar horário'),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
+
+class DayOfWeekTime {
+  final int day;
+  final TimeOfDay time;
+  const DayOfWeekTime(this.day, this.time);
+}
